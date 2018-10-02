672a233f82a30b9b6226ced4f2d3ac40a270617a
Alessio Bazzanella <alessio.bazzanella@me.com>
2018-10-02 19:05:30 +0200

    Porting to python 3

--- library/models.py
+++ library/models.py
@@ -3,10 +3,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import gettext as _
 from django_webix.models import GenericModelWebix
 
 
+@python_2_unicode_compatible
 class Author(GenericModelWebix):
     first_name = models.CharField(max_length=255, verbose_name=_('First name'))
     last_name = models.CharField(max_length=255, verbose_name=_('Last name'))
@@ -21,12 +23,19 @@ class Author(GenericModelWebix):
         url_update = 'author_update'
         url_delete = 'author_delete'
 
+    def __str__(self):
+        return '{first_name} {last_name}'.format(first_name=self.first_name, last_name=self.last_name)
 
+
+@python_2_unicode_compatible
 class Book(models.Model):
     title = models.CharField(max_length=255, verbose_name=_('Title'))
     description = models.TextField(blank=True, null=True, verbose_name=_('Description'))
-    author = models.ForeignKey(Author, verbose_name=_('Author'))
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name=_('Author'))
 
     class Meta:
         verbose_name = _('Book')
         verbose_name_plural = _('Books')
+
+    def __str__(self):
+        return '{author}, {title}'.format(author=self.author, title=self.title)
